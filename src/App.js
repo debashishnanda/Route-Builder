8f6efb36f14221b445b5e64a740e860c666e2d13
debashishnanda <48204005+debashishnanda@users.noreply.github.com>
2020-10-04 17:24:17 +0530

    Route
    Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import RouteBuildingVisualizer from './RouteBuildingVisualizer/RouteBuildingVisualizer';
+
+function App() {
+  return (
+    <div className="App">
+      <RouteBuildingVisualizer></RouteBuildingVisualizer>
+    </div>
+  );
+}
+
+export default App;
